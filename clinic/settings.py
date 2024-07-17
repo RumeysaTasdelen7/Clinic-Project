73e5b7433bcf8f23dc3036b309bc92557a22467f
RumeysaTasdelen7 <rumeysatasdelen06@gmail.com>
2024-07-17 20:33:00 +0300

    17.07.2024

--- clinic/settings.py
+++ clinic/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    #myapp
+    "rmyapp",
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join[BASE_DIR, 'static'],
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
